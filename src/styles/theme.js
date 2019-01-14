ac3e04d9b8f30fd3bb224de5f18186a8fa0887e3
tdkn <web@tedokon.com>
2019-01-15 00:33:51 +0900

    Refactor

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,9 +1,10 @@
-import { createMuiTheme } from '@material-ui/core/styles'
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
 
 const palette = {
   primary: { main: '#263238' },
   secondary: { main: '#00E676' }
 }
+
 const themeName = 'Outer Space Spring Green Raccoon'
 
 export default createMuiTheme({
